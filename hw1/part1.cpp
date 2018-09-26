a043fb1b20eff40ffb7f256e3da6f22f45cfd04a
Henry <henry@reed.edu>
2018-09-26 14:44:57 -0700

    Fixed timing and random indecies; added TODOs

--- hw1/part1.cpp
+++ hw1/part1.cpp
@@ -18,10 +18,16 @@
 #include <string>
 #include <cstdlib>
 
+// TODO: make the indecies and unit buffer size equal to 32 bytes,
+//       so that it can hold it's own indecies
+
 typedef std::chrono::duration<double> duration;
+typedef __int32 bufferunit;
 
 // gets a random int in the range [0..n-1]
-int random_index(int n) { return std::rand() % n; }
+bufferunit random_index(bufferunit n) {
+    return rnd = 0 + int((n * rand()) / (RAND_MAX + 1.0));
+}
 
 // number of trials to use in calculating
 // the average latency
@@ -31,46 +37,54 @@ const int TRIALS = std::pow(2,24);
  * measure_latency:
  * 
  * measures the mean latency in accessing a byte
- * of memory from a `n`-byte buffer.
+ * of memory from a `n`-bufferunit buffer.
  * 
  * `readable` flags the output to be formatted in a
  * human-readable way. otherwise, catered to gnuplot
 */
-void measure_latency(int n, bool readable) {
+void measure_latency(bufferunit n, bool readable) {
     
-    // create a buffer of `n` bytes (chars)
-    std::vector<char> buffer(n);
-    for (int i = 0; i < n; i++) buffer[i] = 'a';
+    // create a buffer of `n` bufferunits
+    std::vector<bufferunit> buffer(n);
+    for (bufferunit i = 0; i < n; i++)
+        buffer[i] = random_index(n);
     
     // store the total of the latencies
     std::chrono::duration<double> total_elapsed_seconds
         = duration::zero();
-    
-    // measure the latency `n` times
+
+    // start timer
+    auto start = std::chrono::steady_clock::now();
+
+    bufferunit index = 0;
+    // measure the latency TRIALS times
     for (int i = 0; i < TRIALS; i++) {
-        // pick a random index to sample,
-        // in order to not let any avaliability
-        // hueristics implemented by the caches
-        // tamper with the measurements
-        unsigned index = random_index(n);
-        
-        // start timer
-        auto start = std::chrono::steady_clock::now();      
-        // access a byte of the buffer
-        buffer.at(index);
-        // end timer
-        auto end = std::chrono::steady_clock::now();
-        // add measured latency to the total
-        total_elapsed_seconds += end-start;
+        // access an entry of the buffer,
+        // also setting the next entry index
+        // (yielded from previous random generation)
+        index = buffer.at(index);
     }
-    
+
+    // end timer
+    auto end = std::chrono::steady_clock::now();
+
+    // calculate average duration
+    auto duration_secs = end - start;
+    auto duration_secs_avg = duration_secs / TRIALS;
+    auto duration_nanosecs_avg = duration_secs_avg * std::pow(10,9);
+
+    // TODO: write outputs
+
+    // TODO: remove the rest of this function
+    // ---------------------------------------
     // calculate average latency
     duration average_elapsed_seconds = total_elapsed_seconds / TRIALS;
     duration average_elapsed_nanosecs = average_elapsed_seconds * std::pow(10,9);
     
     // for a human-readable output format
     if (readable) {
-        std::cout << "size of buffer: " << std::to_string(n/1000) << " kb\n"
+        std::cout << "size of buffer: "
+                        << std::to_string(n/1000) << " kb\n"
                   << "average latency: "
                         << average_elapsed_nanosecs.count() << " ns\n\n";    
     // for a gnuplot-readable output format
@@ -78,10 +92,20 @@ void measure_latency(int n, bool readable) {
         std::cout << std::to_string(n/1000) << " "
                   << average_elapsed_nanosecs.count() << "\n";
     }
+    // ---------------------------------------
 }
 
-const int BUFFER_SIZE_EXP_MIN = 10;
-const int BUFFER_SIZE_EXP_MAX = 26;
+// buffer size constants
+const unsigned BUFFER_SIZE_BYTES_EXP_MIN = 10; // min exponent
+const unsigned BUFFER_SIZE_BYES_EXP_MAX  = 26; // max exponent
+const unsigned BUFFER_UNIT_SIZE_BYTES    = 32; // 32 bytes per entry in buffer
+
+bufferunit buffer_size_bytes_exp_to_buffer_units(unsigned buffer_size_bytes_exp) {
+    // 2^i gives the number of bytes in buffer
+    // 2^i/BUFFER_UNIT_SIZE_BYTES gives the
+    //   number of entries in buffer
+    return std::pow(2,buffer_size_bytes_exp) / BUFFER_UNIT_SIZE_BYTES;
+}
 
 /*
  * measure_range_latencies:
@@ -91,8 +115,11 @@ const int BUFFER_SIZE_EXP_MAX = 26;
  * 
  * see `measure_latency` for description of `readable`
  */
+
+
 void measure_range_latencies(bool readable) {
-    for (int i = BUFFER_SIZE_EXP_MIN; i <= BUFFER_SIZE_EXP_MAX; i++) {
-        measure_latency(std::pow(2,i), readable);
+    for (unsigned i = BUFFER_SIZE_EXP_MIN; i <= BUFFER_SIZE_EXP_MAX; i++) {
+        measure_latency(buffer_size_bytes_exp_to_buffer_units(i),
+            readable);
     }
 }

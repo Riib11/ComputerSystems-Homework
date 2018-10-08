637a61b2a979b472caf6340cadf6d7b1dc621571
riib11 <henry@monkeyface.com>
2018-10-08 13:11:51 -0700

    started hw1

---
+++ hw2/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: henry
+ *
+ * Created on October 8, 2018, 1:09 PM
+ */
+
+#include <cstdlib>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+

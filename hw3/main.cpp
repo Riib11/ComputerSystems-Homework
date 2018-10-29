ce422ab0c9a636fd3db118330b6f9173d30729dc
riib11 <henry@monkeyface.com>
2018-10-29 13:23:51 -0700

    migrated frozen hw2 code to hw3

---
+++ hw3/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+#include <test.h>
+
+using namespace std;
+
+/*
+ * MAIN
+ */
+int main(int argc, char** argv) {
+    
+    unit_test();
+    
+    return 0;
+}
+
